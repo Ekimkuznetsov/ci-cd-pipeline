5bb4302cd46cb49d9b663ccab8f0a9d466c26117
Mykhailo Kuznietsov <mykhailo_kuznietsov@epam.com>
2022-12-26 23:25:58 +0200

    Ansible folder added. Roles: git, docker, jenkins

--- petclinic_ci/modules/instance/variables.tf
+++ petclinic_ci/modules/instance/variables.tf
@@ -7,3 +7,13 @@ variable "subnetwork" {
   description = "A reference (self link) to the subnetwork to host the instance in"
   type        = string
 }
+
+variable "user" {
+  description = "A reference (self link) to the VPC network to host the instance"
+  type        = string
+}
+
+variable "publickeypath" {
+  description = "A reference (self link) to the subnetwork to host the instance in"
+  type        = string
+}
